f026a90361d6b627e09d06ba43f7821d48657701
Gordin <9ordin@gmail.com>
2013-12-01 03:02:10 +0100

    fixed setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir -p tmp/swap tmp/undo tmp/backup vim.old bundle
-for i in ~/.gvim* ~/.vim*; do [ -e $i ] && mv $i ./vim.old/$i; done
+for i in ~/.gvim* ~/.vim*; do [ -e $i ] && mv $i ./vim.old/; done
 git clone https://github.com/gmarik/vundle.git bundle/vundle
+ln -sf `pwd`/vimrc ~/.vimrc 
+ln -sf `pwd`/gvimrc ~/.gvimrc
+ln -sf `pwd` ~/.vim
 vim +BundleInstall! +qall
