8693f41b475a8955ffef814b5171e6cc23d9bca8
Gordin <9ordin@gmail.com>
2013-12-04 10:03:19 +0100

    added spellfiles to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 mkdir -p tmp/swap tmp/undo tmp/backup vim.old bundle
 for i in ~/.gvim* ~/.vim*; do [ -e $i ] && mv $i ./vim.old/; done
 git clone https://github.com/gmarik/vundle.git bundle/vundle
-ln -sf `pwd`/vimrc ~/.vimrc 
+ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gvimrc ~/.gvimrc
 ln -sf `pwd` ~/.vim
 vim +BundleInstall! +qall
+
+# Get spellfiles
+mkdir -p ~/.vim/spell
+cd ~/.vim/spell
+wget http://ftp.vim.org/pub/vim/runtime/spell/{de,en}.utf-8.{spl,sug}
 
 cd ~/.vim/
 bash ycm.sh
