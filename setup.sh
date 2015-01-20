82cd9e75b1eb8b0203cc87e7bcd80f1ba90dd429
Gordin <9ordin@gmail.com>
2015-01-20 17:59:57 +0100

    fixed setupfile

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 mkdir -p tmp/swap tmp/undo tmp/backup vim.old bundle
 for i in ~/.gvim* ~/.vim*; do [ -e $i ] && mv $i ./vim.old/; done
-git clone https://github.com/gmarik/vundle.git bundle/vundle
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gvimrc ~/.gvimrc
 ln -sf `pwd` ~/.vim
-vim +BundleInstall! +qall
 
 # Get spellfiles
 mkdir -p ~/.vim/spell
 cd ~/.vim/spell
 wget http://ftp.vim.org/pub/vim/runtime/spell/{de,en}.utf-8.{spl,sug}
 
-cd ~/.vim/
-bash ycm.sh
-
-cd ~/.vim/
-bash tern.sh
+# Get plugins
+mkdir -p ~/.vim/autoload
+curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
